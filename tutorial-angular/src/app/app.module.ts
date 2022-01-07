8af9954fc257ef3eb798c6fa10b0264936becc17
Adil Akhazzan <61242543+BlancoZX@users.noreply.github.com>
2022-01-07 19:08:41 +0100

    Cosas

--- tutorial-angular/src/app/app.module.ts
+++ tutorial-angular/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GamesComponent } from './games/games.component';
-import { GameComponent } from './game/game.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    GamesComponent,
-    GameComponent
+
   ],
   imports: [
     BrowserModule,
