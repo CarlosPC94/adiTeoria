456fd4923aa96a43b5b45cde8a600e6948a4c16a
Carlos Poveda Cañizares <cpc73@alu.ua.es>
2022-01-07 17:48:25 +0100

    Añadido proyecto Angular

---
+++ tutorial-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GamesComponent } from './games/games.component';
+import { GameComponent } from './game/game.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GamesComponent,
+    GameComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
